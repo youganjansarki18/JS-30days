2ee4fe9960f060b05fa52b3ae23c4b661a8861f9
Youganjan Sarki <youganjansarki2@gmail.com>
2021-10-16 11:21:55 +0530

    commit

---
+++ Project-6-Ajax Type Ahead/main.js
@@ -0,0 +1,32 @@
+const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const cities=[];
+fetch(endpoint)
+   .then(blob=>blob.json())
+   .then(data=>cities.push(...data));
+//Now, the cities array contains the data of all the cities of USA.
+//We need to compare the input words with the cities 
+//So, we will use the function for checking it
+function check(wordstomatch,cities){
+    return cities.filter(place =>{
+        const regex= new RegExp(wordstomatch,'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+function display_matches(){
+    const matched_data= check(this.value, cities);
+    console.log(matched_data);
+    const html= matched_data.map(place=>{
+        return `
+          <li>
+              <span class="name">${place.city},${place.state}</span>
+              <span class="population">${place.population}</span>
+          </li>
+        `;
+    }).join('');
+    console.log(html);
+    let suggest= document.querySelector('.suggestions');
+    suggest.innerHTML= html;
+}
+const searchInput=document.querySelector('.search');
+// searchInput.addEventListener('change',display_matches);
+searchInput.addEventListener('keyup',display_matches);
